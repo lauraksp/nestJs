0f81c29f4f605746331d63fe95ac682bd313322d
lauraksp@icloud.com <lauraksp@icloud.comclear>
2023-03-05 00:53:09 -0300

    anotacoes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('prefixo') //pode passar prefixo
+@Controller('prefixo') /*pode passar prefixo*/
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
+import { DtoService } from './dto/dto.service';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [],
+  imports: [TweetsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService, DtoService],
 })
 export class AppModule {}
